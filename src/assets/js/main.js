d8469e2c4a9e5be40beb46a5b5b6a849613b0f4c
Luan Gjokaj <luan@riangle.com>
2022-08-24 12:59:17 +0200

    feat: animations poc

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,14 +3,28 @@ gsap.registerPlugin(ScrollTrigger);
 const tl = gsap.timeline({
 	smoothChildTiming: true,
 	scrollTrigger: {
-		trigger: ".test",
+		trigger: "#first-container",
 		markers: false,
 	},
 });
 
-tl.to(".test", {
+tl.to("#title-1", {
 	opacity: 1,
 	y: 0,
 	x: 0,
 	delay: 0,
-});
+}, 0.1);
+
+tl.to("#di-logo", {
+	opacity: 1,
+	y: 0,
+	x: 0,
+	delay: 0,
+}, 0.2);
+
+tl.to("#title-2", {
+	opacity: 1,
+	y: 0,
+	x: 0,
+	delay: 0,
+}, 0.3);
